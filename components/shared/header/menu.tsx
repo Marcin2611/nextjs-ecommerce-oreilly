efc76f98ca0572af852574def3701275099ee15e
Marcin <marcin.golec1995@gmail.com>
2025-05-15 16:14:19 +0200

    sign out added

--- components/shared/header/menu.tsx
+++ components/shared/header/menu.tsx
@@ -1,43 +1,36 @@
 import ModeToggle from "@/components/shared/header/mode-toggle";
-import {Button} from "@/components/ui/button";
+import { Button } from "@/components/ui/button";
+import { Sheet, SheetContent, SheetDescription, SheetTitle, SheetTrigger } from "@/components/ui/sheet";
+import { EllipsisVertical, ShoppingCart } from "lucide-react";
 import Link from "next/link";
-import {EllipsisVertical, ShoppingCart, UserIcon} from "lucide-react";
-import {Sheet, SheetContent, SheetDescription, SheetTitle, SheetTrigger} from "@/components/ui/sheet";
+import UserButton from "./user-button";
 
 const Menu = () => {
     return (
         <div className="flex justify-end gap-3">
             <nav className="hidden md:flex w-full max-w-xs gap-1">
-                <ModeToggle/>
+                <ModeToggle />
                 <Button asChild variant="ghost">
                     <Link href="/cart">
-                        <ShoppingCart/> Cart
+                        <ShoppingCart /> Cart
                     </Link>
                 </Button>
-                <Button asChild>
-                    <Link href="/sign-in">
-                        <UserIcon/> Sign In
-                    </Link>
-                </Button>
+                <UserButton />
             </nav>
             <nav className="md:hidden">
                 <Sheet>
                     <SheetTrigger className="align-middle">
-                        <EllipsisVertical/>
+                        <EllipsisVertical />
                     </SheetTrigger>
                     <SheetContent className="flex flex-col items-start p-6">
-                            <SheetTitle>Menu</SheetTitle>
-                        <ModeToggle/>
+                        <SheetTitle>Menu</SheetTitle>
+                        <ModeToggle />
                         <Button asChild variant="ghost">
                             <Link href="/cart">
-                                <ShoppingCart/> Cart
+                                <ShoppingCart /> Cart
                             </Link>
                         </Button>
-                        <Button asChild>
-                            <Link href="/sign-in">
-                                <UserIcon/> Sign In
-                            </Link>
-                        </Button>
+                        <UserButton />
                         <SheetDescription></SheetDescription>
                     </SheetContent>
                 </Sheet>
