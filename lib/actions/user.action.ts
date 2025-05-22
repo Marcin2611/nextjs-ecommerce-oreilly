617b5004b9a8bcf604430067b29ebe1ee46fa61e
Marcin2611 <marcin.golec1995@gmail.com>
2025-05-22 08:10:19 +0200

    payment method form finished

--- lib/actions/user.action.ts
+++ lib/actions/user.action.ts
@@ -1,13 +1,13 @@
 'use server';
 
-import {shippingAddressSchema, signInFormSchema, signUpFormSchema} from "../validators";
+import {paymentMethodSchema, shippingAddressSchema, signInFormSchema, signUpFormSchema} from "../validators";
 import {auth, signIn, signOut} from "@/auth";
-import { isRedirectError } from "next/dist/client/components/redirect-error";
-import { hashSync } from "bcrypt-ts-edge";
-import { prisma } from "@/db/prisma";
-import { formatError } from "../utils";
-import shippingAddressForm from "@/app/(root)/shipping-address/shipping-address-form";
+import {isRedirectError} from "next/dist/client/components/redirect-error";
+import {hashSync} from "bcrypt-ts-edge";
+import {prisma} from "@/db/prisma";
+import {formatError} from "../utils";
 import {ShippingAddress} from "@/types";
+import { z } from "zod";
 
 export async function signInWithCredentials(prevState: unknown, formData: FormData) {
     try {
@@ -96,6 +96,31 @@ export async function updateUserAddress(data: ShippingAddress) {
         });
 
         return { success: true, message: 'User updated successfully' }
+    } catch (error) {
+        return { success: false, message: formatError(error)}
+    }
+}
+
+export async function updateUserPaymentMethod(data: z.infer<typeof paymentMethodSchema>) {
+    try {
+        const session = await auth();
+        const currentUser = await prisma.user.findFirst({
+            where: { id: session?.user?.id }
+        });
+
+        if (!currentUser) throw new Error('User not found');
+
+        const paymentMethod = paymentMethodSchema.parse(data);
+
+        await prisma.user.update({
+            where: { id: currentUser.id },
+            data: { paymentMethod: paymentMethod.type }
+        });
+
+        return {
+            success: true,
+            message: 'User updated successfully'
+        }
     } catch (error) {
         return { success: false, message: formatError(error)}
     }
